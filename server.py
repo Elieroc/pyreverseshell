ab5bb28a3c8935ad8c020946f2f8bebbedee6a19
Elieroc <34548349+Elieroc@users.noreply.github.com>
2021-01-09 17:47:25 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,25 @@
 import socket
+import sys
 
 host, port = ('', 6666)
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.bind((host, port))
-print('Le serveur est démarré !')
+print('Le serveur est démarré !'+"\n")
 
 while True:
     socket.listen()
     conn, address = socket.accept()
-    print("Un client vient de se connecter")
+    print("/!\ Client connecté /!\ ")
+    while True:
+        print("Envoyer une commande :")
+        data=str( sys.stdin.readline() )
+        data = data.encode("utf8")
+        conn.send(data)
 
-    data = conn.recv(1024)
-    data = data.decode("utf8")
-    print(data)
+        data = conn.recv(9999)
+        data = str( data.decode("utf-8", errors="ignore"))
+        print("Message reçu :\n"+data)
 
 conn.close()
 socket.close()
